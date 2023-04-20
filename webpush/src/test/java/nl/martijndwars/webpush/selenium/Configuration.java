34fe552a6df54dce46f77c1aac82cf19d6e60235
Andre Blanke <blanke.and@gmail.com>
2023-04-20 11:45:45 +0200

    Remove broken Selenium-based tests

--- webpush/src/test/java/nl/martijndwars/webpush/selenium/Configuration.java
+++
@@ -1,8 +0,0 @@
-package nl.martijndwars.webpush.selenium;
-
-public record Configuration(String browser, String version, String publicKey, String gcmSenderId) {
-
-    public boolean isVapid() {
-        return (publicKey() != null) && !publicKey().isEmpty();
-    }
-}
